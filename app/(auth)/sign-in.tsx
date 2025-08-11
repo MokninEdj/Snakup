4b7b0b0b679e8d32659b2e115e182fcaa4b6a45b
MokninEdj <elhadjmoknine@gmail.com>
2025-08-11 20:08:35 +0100

    built the sign up and sign in functionalities

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,57 +1,65 @@
-import {View, Text, Alert} from 'react-native'
-import React, {useState} from 'react'
-import CustomInput from "@/components/CustomInput";
 import CustomButton from "@/components/CustomButton";
-import {Link, router} from "expo-router"
+import CustomInput from "@/components/CustomInput";
+import { signIn } from "@/lib/appwrite";
+import { Link, router } from "expo-router";
+import React, { useState } from "react";
+import { Alert, Text, View } from "react-native";
 const SignIn = () => {
-    const [isSubmitting,setIsSubmitting]=useState(false)
-    const [form,setForm]=useState({email:"",password:""})
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [form, setForm] = useState({ email: "", password: "" });
 
-    const onSubmit=async ()=>{
-        if(!form.email||!form.password) return Alert.alert('Error',"Please enter a valid Email & Password")
+  const { email, password } = form;
+  const onSubmit = async () => {
+    if (!email || !password)
+      return Alert.alert("Error", "Please enter a valid Email & Password");
 
-        setIsSubmitting(true)
-        try {
-            //Call api
-            Alert.alert('Success',"User signed In successfully")
-            router.replace('/')
-        }catch (error:any){
-            Alert.alert('Error',error.message)
-
-        }finally {
-            setIsSubmitting(false)
-        }
+    setIsSubmitting(true);
+    try {
+      //Call api
+      await signIn({ email, password });
+      Alert.alert("Success", "User signed In successfully");
+      router.replace("/");
+    } catch (error: any) {
+      Alert.alert("Error", error.message);
+    } finally {
+      setIsSubmitting(false);
     }
+  };
 
-    return (
-        <View className={"gap-10 bg-white p-5 mt-5"}>
-            <CustomInput
-                label={"Email"}
-                keyboardType={"email-address"}
-                placeholder={"Enter your email"}
-                value={form.email}
-                onChangeText={(text)=>{setForm(prev=>({...prev,email: text}))}
-
-                }/>
-            <CustomInput
-                label={"Password"}
-                secureTextEntry={true}
-                placeholder={"Enter your Password"}
-                value={form.password}
-                onChangeText={(text)=>{setForm((prev=>({...prev,password: text})))}
-
-                }/>
-            <CustomButton title={"Sign in"} isLoading={isSubmitting} onPress={onSubmit}/>
-            <View className={"flex justify-center flex-row gap-2 mt-5"}>
-                <Text className={"base-regular text-gray-100"}>
-                    Don't have an account?
-                </Text>
-                <Link href={"/sign-up"} className={"base-bold text-primary"}>
-                    Sign Up
-                </Link>
-
-            </View>
-        </View>
-    )
-}
-export default SignIn
+  return (
+    <View className={"gap-10 bg-white p-5 mt-5"}>
+      <CustomInput
+        label={"Email"}
+        keyboardType={"email-address"}
+        placeholder={"Enter your email"}
+        value={email}
+        onChangeText={(text) => {
+          setForm((prev) => ({ ...prev, email: text }));
+        }}
+      />
+      <CustomInput
+        label={"Password"}
+        secureTextEntry={true}
+        placeholder={"Enter your Password"}
+        value={password}
+        onChangeText={(text) => {
+          setForm((prev) => ({ ...prev, password: text }));
+        }}
+      />
+      <CustomButton
+        title={"Sign in"}
+        isLoading={isSubmitting}
+        onPress={onSubmit}
+      />
+      <View className={"flex justify-center flex-row gap-2 mt-5"}>
+        <Text className={"base-regular text-gray-100"}>
+          Don&apos; have an account?
+        </Text>
+        <Link href={"/sign-up"} className={"base-bold text-primary"}>
+          Sign Up
+        </Link>
+      </View>
+    </View>
+  );
+};
+export default SignIn;
--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,7 +1,80 @@
+import { CreateUserParams, SignInParams } from "@/types";
+import {
+  Account,
+  Avatars,
+  Client,
+  Databases,
+  ID,
+  Query,
+} from "react-native-appwrite";
+
 export const appwriteConfig = {
-    endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
-    projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
-    platform: "com.moknine.snakup",
-    databaseId:"688be26800181c60b090",
-    userCollectionId:"688be2e4003be00665b6"
-}
+  endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT!,
+  projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!,
+  platform: "com.moknine.snakup",
+  databaseId: "688be26800181c60b090",
+  userCollectionId: "689a396b002022c355d1",
+};
+
+const client = new Client();
+
+client
+  .setEndpoint(appwriteConfig.endpoint)
+  .setProject(appwriteConfig.projectId)
+  .setPlatform(appwriteConfig.platform);
+
+export const account = new Account(client);
+
+export const database = new Databases(client);
+export const avatars = new Avatars(client);
+
+// Register new user
+export const createUser = async ({
+  email,
+  password,
+  name,
+}: CreateUserParams) => {
+  try {
+    const user = await account.create(ID.unique(), email, password, name);
+    if (!user) throw new Error("User not created");
+    signIn({ email, password });
+    const avatar = avatars.getInitialsURL(name);
+    return database.createDocument(
+      appwriteConfig.databaseId,
+      appwriteConfig.userCollectionId,
+      ID.unique(),
+      {
+        accountId: user.$id,
+        name,
+        email,
+        avatar,
+      }
+    );
+  } catch (error) {
+    throw new Error(error as string);
+  }
+};
+
+// Sign in user
+export const signIn = async ({ email, password }: SignInParams) => {
+  try {
+    await account.createEmailPasswordSession(email, password);
+  } catch (error) {
+    throw new Error(error as string);
+  }
+};
+
+export const getCurrentUser = async () => {
+  try {
+    const currentUser = await account.get();
+    if (!currentUser) throw new Error("User not found");
+    const userData = await database.listDocuments(
+      appwriteConfig.databaseId,
+      appwriteConfig.userCollectionId,
+      [Query.equal("accountId", currentUser.$id)]
+    );
+    return userData.documents[0];
+  } catch (error) {
+    throw new Error(error as string);
+  }
+};
